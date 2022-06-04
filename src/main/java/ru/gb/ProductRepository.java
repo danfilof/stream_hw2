d7beaec58a20e86b32e6ef8f91db264232203f8e
danfilof <danila.anfilofyev@gmail.com>
2022-06-04 21:03:58 +0200

    First task completed. Although instead of List used map, as long as I want every single unit in the repository to have unique id

--- src/main/java/ru/gb/ProductRepository.java
+++ src/main/java/ru/gb/ProductRepository.java
@@ -4,14 +4,20 @@ import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Component
 public class ProductRepository {
 
-    private List<Product> products;
+    private final Map<Long, Product> productMap = new ConcurrentHashMap<>();
 
     @PostConstruct
     private void init() {
-
+        productMap.put(1L, new Product(14,"Coconut", 10));
+        productMap.put(2L, new Product(22,"Pineapple", 12));
+        productMap.put(3L, new Product(14,"Coconut", 10));
+        productMap.put(4L, new Product(37,"Milk", 2));
+        productMap.put(5L, new Product(98,"Butter", 3.5f));
     }
 }
